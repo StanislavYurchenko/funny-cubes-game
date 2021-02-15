1b58997fc1e632d31efc7106820d5f194e6f2102
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-02-15 12:05:37 +0200

    fix errpr page

--- backend/routes/error_page.js
+++ backend/routes/error_page.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // ERROR-PAGE
-router.use('/error-page', async (req, res) => {
+router.use('/', async (req, res) => {
   res.type('.html');
   const backRoute = req.session.backRoute ?? '/';
   const message = req.session.errorMessage ?? 'Operation canceled. Try again!';
